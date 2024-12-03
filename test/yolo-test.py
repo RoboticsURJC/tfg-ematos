3c718e2d0e7775aabeb58e3c5c3201bf7f62db1a
Elisa Jazmin Matos Juarez <ej.matos.2020@alumnos.urjc.es>
2024-12-03 20:01:56 +0100

    boundin boxes

--- test/yolo-test.py
+++ test/yolo-test.py
@@ -27,14 +27,31 @@ while True:
 
     # Predict with the model
     results = model(frame)  # predict on camera video
+
+    # Extract bounding boxes,labels and confidence
+    boxes = results[0].boxes
+    for box in boxes:
+      x1, y1, x2, y2 = box.xyxy[0]  # Extract coordinates and confidence
+      conf = box.conf[0] # confidence score
+      cls = int(box.cls)
+      
+      if conf > 0.5:
+        print("Detect somthing")
+        label = f"{model.names[cls]} {conf:.2f}"  # Get class name and confidence
+   
+        cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+        # Put label on top of the bounding box
+        cv2.putText(frame, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
     
+
+    cv2.imshow("Detection", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
       break
     
 
 
-# Liberar la cámara y cerrar las ventanas
+# free the camera and close windows
 cap.release()
 cv2.destroyAllWindows()
 
