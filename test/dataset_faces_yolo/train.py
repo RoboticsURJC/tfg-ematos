61c3057de081432f8c6f67c5663c9c167a26cfbe
Elisa Jazmin Matos Juarez <ej.matos.2020@alumnos.urjc.es>
2025-03-03 14:14:07 +0100

    rm train and dataset

--- test/dataset_faces_yolo/train.py
+++ test/dataset_faces_yolo/train.py
@@ -1,9 +1,12 @@
+import ultralytics
 from ultralytics import YOLO
 
-model = YOLO("yolo11n.pt")
+model = YOLO("yolov8n.pt")
 
+ultralytics.checks()
 results = model.train(data="../dataset_faces_yolo/data.yaml", 
                       epochs=10, 
                       imgsz=640, 
+                      batch=8,
                       plots=True,
                       ) 
