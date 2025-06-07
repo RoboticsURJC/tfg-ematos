97f40f49e320ca47450538e2d7f16354c0778747
Elisajmj <ej.matos.2020@alumnos.urjc.es>
2025-06-07 21:31:42 +0200

    almost work recognition raspberry

---
+++ test/recognition-models/mobile-facenet/mobile-facenet-test.py
@@ -0,0 +1,151 @@
+import cv2
+import pickle
+import numpy as np
+import tflite_runtime.interpreter as tflite
+from picamera2 import Picamera2, Preview
+from numpy.linalg import norm
+
+# Load TFLite model and allocate tensors for detection and recogniton.
+try:
+    detector = tflite.Interpreter(
+        model_path="face_detection_front.tflite",
+        experimental_delegates=[tflite.load_delegate('libedgetpu.so.1')]
+    )
+
+    recognizer = tflite.Interpreter(
+        model_path="mobilefacenet.tflite",
+        experimental_delegates=[tflite.load_delegate('libedgetpu.so.1')]
+    )
+    print("Edge TPU detected and working")
+except Exception as e:
+    print("Error at charging Edge TPU:", e)
+    detector = tflite.Interpreter(model_path="face_detection_front.tflite")
+    recognizer = tflite.Interpreter(model_path="face_detection_front.tflite")
+
+detector.allocate_tensors()
+recognizer.allocate_tensors()
+
+
+# Get input and output tensors.
+input_details_detector = detector.get_input_details()
+output_details_detector = detector.get_output_details()
+input_shape_detector = input_details_detector[0]['shape']
+
+input_details_recognizer = recognizer.get_input_details()
+output_details_recognizer = recognizer.get_output_details()
+input_shape_recognizer = input_details_recognizer[0]['shape']
+
+picam2 = Picamera2()
+picam2.preview_configuration.main.size =  (1280, 720)
+picam2.preview_configuration.main.format = "RGB888"
+picam2.configure("preview")
+picam2.start()
+
+# load known faces embeddings
+with open('known_faces.pkl', 'rb') as f:
+    known_face_names, known_face_encodings = pickle.load(f)
+
+known_faces_dict = {}
+for name, encoding in zip(known_face_names, known_face_encodings):
+   
+    if name in known_faces_dict:
+        known_faces_dict[name].append(encoding)
+    else:
+        known_faces_dict[name] = [encoding]
+
+def cosine_similarity(a, b):
+    return np.dot(a, b) / (norm(a) * norm(b))
+
+def preprocess_face(face_img):
+    img = cv2.resize(face_img, (input_shape_recognizer[2], input_shape_recognizer[1]))  # (width, height)
+    img = img.astype(np.float32) / 255.0
+    return np.expand_dims(img, axis=0)
+
+def get_embedding(face_img):
+    input_data = preprocess_face(face_img)
+    recognizer.set_tensor(input_details_recognizer[0]['index'], input_data)
+    recognizer.invoke()
+    embedding = recognizer.get_tensor(output_details_recognizer[0]['index'])[0]
+    embedding = embedding / np.linalg.norm(embedding)
+    return embedding
+
+def recognize_face(embedding, known_faces_dict, threshold=0.5):
+    best_match = None
+    best_score = -1
+    for name, embeddings in known_faces_dict.items():
+        for e in embeddings:
+            if embedding.shape != e.shape:
+                print(f"Warning: Formas no coinciden: embedding {embedding.shape}, e {e.shape}")
+                continue
+            score = cosine_similarity(embedding, e)
+            if score > best_score:
+                best_score = score
+                best_match = name
+    if best_score > threshold:
+        return best_match, best_score
+    else:
+        return None, best_score
+    
+while True:
+
+    frame = picam2.capture_array()
+    frame = cv2.flip(frame, 1)
+    h, w, _ = frame.shape
+
+    img_resized = cv2.resize(frame, (input_shape_detector[2], input_shape_detector[1]))
+
+    # Convert the image to numpy array
+    input_data = np.expand_dims(img_resized.astype(np.float32), axis=0)
+
+    # normalize 
+    input_data = input_data / 255.0
+
+    # Set the tensor to point to the input to be inferred 
+    detector.set_tensor(input_details_detector[0]['index'], input_data)
+
+    # run the inference
+    detector.invoke()
+
+    detections = detector.get_tensor(output_details_detector[0]['index'])[0]
+    confidences = detector.get_tensor(output_details_detector[1]['index'])[0]
+
+    
+    for i in range(len(confidences)):
+        if confidences[i] > 0.6:
+            print(f"Person Detect {i}")
+          
+            ymin, xmin, ymax, xmax = detections[i][:4]
+            ymin, xmin = max(0, ymin), max(0, xmin)
+            ymax, xmax = min(1, ymax), min(1, xmax)
+            x1, y1 = int(xmin * w), int(ymin * h)
+            x2, y2 = int(xmax * w), int(ymax * h)
+
+            # Validar y ordenar coords
+            x1, x2 = sorted([x1, x2])
+            y1, y2 = sorted([y1, y2])
+
+            x1 = max(0, min(x1, w - 1))
+            x2 = max(0, min(x2, w - 1))
+            y1 = max(0, min(y1, h - 1))
+            y2 = max(0, min(y2, h - 1))
+
+            if (x2 - x1) < 10 or (y2 - y1) < 10:
+                continue
+
+            # get face frame
+            face_img = frame[y1:y2, x1:x2]
+            embedding = get_embedding(face_img)
+
+            person_name, score = recognize_face(embedding, known_faces_dict, threshold=0.5)
+            label = f"{person_name} ({score:.2f})" if person_name else "Desconocido"
+            print(f"You are: {label}")
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(frame, label, (x1, y1-10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
+
+            
+    cv2.imshow("Recognitiom", frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.destroyAllWindows()
