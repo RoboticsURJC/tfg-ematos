87695253e67fca4811a599464e66f625e52dfe9b
Elisa Jazmin Matos Juarez <ej.matos.2020@alumnos.urjc.es>
2026-01-09 21:49:26 +0100

    fix eyes

--- eyes/eyes.py
+++ eyes/eyes.py
@@ -3,20 +3,17 @@ import digitalio
 from PIL import Image, ImageDraw
 import adafruit_ili9341
 
-# SPI
-spi = board.SPI()
+# Pines
+cs_pin = digitalio.DigitalInOut(board.D8)   # CS PIN 24
+dc_pin = digitalio.DigitalInOut(board.D23)  # DC PIN 16
+reset_pin = digitalio.DigitalInOut(board.D24) # RESET PIN 18
 
-# Pines según tu conexión
-cs = digitalio.DigitalInOut(board.D8)
-dc = digitalio.DigitalInOut(board.D23)
-rst = digitalio.DigitalInOut(board.D24)
-
-# Pantalla
+# Inicializar pantalla
 display = adafruit_ili9341.ILI9341(
-    spi,
-    cs=cs,
-    dc=dc,
-    rst=rst,
+    board.SPI(),
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
     width=240,
     height=320,
     rotation=180
@@ -34,5 +31,5 @@ draw.ellipse((75, 135, 95, 155), fill="black")
 draw.ellipse((160, 100, 240, 180), fill="white")
 draw.ellipse((195, 135, 215, 155), fill="black")
 
-# Mostrar
+# Mostrar imagen
 display.image(image)
