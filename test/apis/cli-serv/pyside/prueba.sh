615003cdf413fbf563af919fb4c420024069a0be
Elisajmj <ej.matos.2020@alumnos.urjc.es>
2026-06-15 12:38:42 +0200

    rename folders

--- test/apis/cli-serv/pyside/prueba.sh
+++
@@ -1,5 +0,0 @@
-for i in {1..5}; do
-    echo "Ejecucion $i ..."
-    python3 latency_test.py
-    sleep 10
-done
--- test/voice/test_whisper.py
+++
@@ -1,9 +0,0 @@
-import whisper
-
-model = whisper.load_model("base")
-result = model.transcribe("test.wav", language="en")
-
-print("Texto detectado:")
-print(result["text"])
-
-
